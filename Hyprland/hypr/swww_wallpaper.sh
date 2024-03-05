e81ea06c4c361634e55b80b78c53ed9ec6688c56
Retr0 <subhamsasmal02gmail.com>
2024-03-05 18:03:46 +0530

    Hyprland and waybar dot files added

---
+++ Hyprland/hypr/swww_wallpaper.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DIR="/home/retro/Pictures/swww/wallpapers"
+
+swww query 2> /dev/null 1> /dev/null
+
+if [ $? -ne 0 ]
+then
+	swww init
+fi
+
+#swww clear-cache
+
+IMG_DAVIDLUCY="davidxlucy"
+IMG_YELAN="yelan"
+IMG_DITF="DITF"
+IMG="northlight"
+
+if [ -z "$1" ]
+then
+	break
+else
+	IMG_YELAN=$1
+fi
+
+WALLPAPER="${DIR}/${IMG}.jpg"
+echo "$WALLPAPER"
+
+swww img --resize fit --transition-duration 3 --transition-angle 45 --transition-fps 30 --transition-type any ${DIR}/${IMG}.jpg
+
+if [ $? -ne 0 ]
+then
+	exit
+else
+	echo "[+] Wallpaper changed"
+fi
---
+++ Waybar/waybar/bin/start.sh
@@ -0,0 +1,42 @@
+############################################################################################################
+
+# This script will start or restart waybar and also make sure that waybar is only visible on the primary monitor.
+
+# Terminate already running bar instances
+killall -q waybar
+
+check() {
+	command -v "$1" >/dev/null 2>&1
+}
+
+notify() {
+	check notify-send && notify-send "$@" || echo "$@"
+}
+
+check hyprctl || {
+	notify "hyprctl is not present"
+	exit 1
+}
+
+data="$(hyprctl monitors -j)"
+laptop="$(hyprctl monitors -j | jq '.[] | select(.id == 0) | .name')"
+readarray -t monitors <<<"$(hyprctl monitors -j | jq '.[] | select(.id != 0) | .name')"
+monitor="${monitors[-1]}"
+
+if [ -z "$monitor" ]; then
+	monitor="$laptop"
+fi
+
+cat <<EOF >"$HOME"/.config/waybar/config
+
+[
+  {
+  "output": [ $monitor ],
+    "include": [
+      "~/.config/waybar/config.jsonc",
+    ],
+  }
+]
+EOF
+
+setsid waybar &>/dev/null &
